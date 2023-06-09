e34fc4854df716a491a388f968abfea85ed81e6a
yasinarafat9 <yasinarafat9612@gmail.com>
2023-06-09 11:13:41 +0600

    get data of classes successfully from mongodb

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ app.use(express.json());
 
 // Mongodb Driver
 
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.nosbxzl.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nosbxzl.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,12 +26,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const classCollection = client.db("summerCampPhotographyDB").collection("classes");
+    
+
+    app.get('/classes', async(req, res) => {
+        const result = await classCollection.find().toArray();
+        res.send(result);
+    })
+ 
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
